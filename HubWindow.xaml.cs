eb1381cac37e0693a4d49ee9776802938e753883
John Schneider <a1steaksa@users.noreply.github.com>
2023-11-19 14:31:21 -0600

    Added .editorconfig Updated config files for project's initial state Added basic README info Renamed MainWindow to HubWindow

--- HubWindow.xaml.cs
+++ HubWindow.xaml.cs
@@ -15,22 +15,17 @@ using Microsoft.UI.Xaml.Navigation;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
 
-// To learn more about WinUI, the WinUI project structure,
-// and more about our project templates, see: http://aka.ms/winui-project-info.
-
 namespace ToolHub;
-/// <summary>
-/// An empty window that can be used on its own or navigated to within a Frame.
-/// </summary>
-public sealed partial class MainWindow : Window
-{
-    public MainWindow()
-    {
-        this.InitializeComponent();
-    }
 
-    private void myButton_Click(object sender, RoutedEventArgs e)
-    {
-        myButton.Content = "Clicked";
-    }
+/// <summary>
+/// The main window of the tool hub.
+/// </summary>
+public sealed partial class HubWindow : Window {
+	public HubWindow() {
+		this.InitializeComponent();
+	}
+
+	private void myButton_Click( object sender, RoutedEventArgs e ) {
+		myButton.Content = "Clicked";
+	}
 }
